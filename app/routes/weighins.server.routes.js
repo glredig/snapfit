7f076bcf4ae669306cfe18e337566936f77125d6
Gabriel Redig <glredig@Gabriels-MBP-3.home>
2015-10-10 02:01:20 -0700

    Add crud controller and all actions

--- app/routes/weighins.server.routes.js
+++ app/routes/weighins.server.routes.js
@@ -2,8 +2,16 @@
 
 module.exports = function(app) {
   var weighins = require('../../app/controllers/weighins.server.controller');
+  var users	= require('../../app/controllers/users.server.controller');
 
 	app.route('/weighins')
         .get(weighins.list)
-        .post(weighins.create);
+        .post(users.requiresLogin, weighins.create);
+
+    app.route('/weighin/:weighinId')
+    	.get(weighins.read)
+    	.put(users.requiresLogin, weighins.update)
+    	.delete(users.requiresLogin, weighins.delete);
+
+    app.param('weighinId', weighins.weighinById);
 };
